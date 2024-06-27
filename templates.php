33b1d6d60e92bba02ef25cb9ca1e223267fb7608
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-06-28 02:49:16 +0530

    Style Updated

--- templates.php
+++ templates.php
@@ -4,9 +4,12 @@
  */
 ?>
 <script type="text/x-template" id="my-template">
-    <div class="counter">
-        <button @click="decrement">-</button>
-        <span>{{ count }}</span>
-        <button @click="increment">+</button>
+    <div class="main-container">
+        <div class="counter">
+            <button @click="decrement">-</button>
+            <span>{{ count }}</span>
+            <button @click="increment">+</button>
+        </div>
     </div>
+
 </script>
